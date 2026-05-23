2ffef432989e3a358d65b15d7c6be65f8f272cfd
rian144hz <rianmatheus669@gmail.com>
2026-05-23 16:13:24 -0300

    Configurando o ambiente jdbc

--- src/db/DB.java
+++ src/db/DB.java
@@ -41,7 +41,7 @@ public class DB {
 
     private static Properties loadProperties() {
 
-        try (FileInputStream fs = new FileInputStream("config/db.properties")) {
+        try (FileInputStream fs = new FileInputStream("db.properties")) {
             Properties props = new Properties();
             props.load(fs);
             return props;
